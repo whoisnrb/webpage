8c48c4dbb4a1a2056a78ce3febe8f49942cb76c7
whoisnrb <whoisnrb@gmail.com>
2025-12-01 17:23:34 +0100

    Refactor auth config for Edge compatibility

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,9 +1,7 @@
 import GitHub from "next-auth/providers/github"
 import Credentials from "next-auth/providers/credentials"
 import type { NextAuthConfig } from "next-auth"
-import bcrypt from "bcryptjs"
 import { LoginSchema } from "@/schemas"
-import { getUserByEmail } from "@/data/user"
 
 const githubId = process.env.GITHUB_CLIENT_ID
 const githubSecret = process.env.GITHUB_CLIENT_SECRET
@@ -20,22 +18,6 @@ export default {
         }),
         Credentials({
             async authorize(credentials) {
-                const validatedFields = LoginSchema.safeParse(credentials);
-
-                if (validatedFields.success) {
-                    const { email, password } = validatedFields.data;
-
-                    const user = await getUserByEmail(email);
-                    if (!user || !user.password) return null;
-
-                    const passwordsMatch = await bcrypt.compare(
-                        password,
-                        user.password
-                    );
-
-                    if (passwordsMatch) return user;
-                }
-
                 return null;
             }
         })
--- src/auth.ts
+++ src/auth.ts
@@ -2,9 +2,37 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/lib/db"
 import authConfig from "./auth.config"
+import Credentials from "next-auth/providers/credentials"
+import bcrypt from "bcryptjs"
+import { LoginSchema } from "@/schemas"
+import { getUserByEmail } from "@/data/user"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
     session: { strategy: "jwt" },
     ...authConfig,
+    providers: [
+        ...authConfig.providers.filter((provider: any) => provider.id !== "credentials"),
+        Credentials({
+            async authorize(credentials) {
+                const validatedFields = LoginSchema.safeParse(credentials);
+
+                if (validatedFields.success) {
+                    const { email, password } = validatedFields.data;
+
+                    const user = await getUserByEmail(email);
+                    if (!user || !user.password) return null;
+
+                    const passwordsMatch = await bcrypt.compare(
+                        password,
+                        user.password
+                    );
+
+                    if (passwordsMatch) return user;
+                }
+
+                return null;
+            }
+        })
+    ]
 })
