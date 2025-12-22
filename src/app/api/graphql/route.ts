d80bdd039734d899d6130772cd4016498ad94b1f
whoisnrb <whoisnrb@gmail.com>
2025-12-22 17:12:08 +0100

    fix: graphql route type error

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from "@apollo/server"
 import { startServerAndCreateNextHandler } from "@as-integrations/next"
 import { typeDefs } from "@/graphql/type-defs"
 import { resolvers } from "@/graphql/resolvers"
+import { NextRequest } from "next/server"
 
 const server = new ApolloServer({
     typeDefs,
@@ -10,4 +11,10 @@ const server = new ApolloServer({
 
 const handler = startServerAndCreateNextHandler(server)
 
-export { handler as GET, handler as POST }
+export async function GET(request: NextRequest) {
+    return handler(request)
+}
+
+export async function POST(request: NextRequest) {
+    return handler(request)
+}
